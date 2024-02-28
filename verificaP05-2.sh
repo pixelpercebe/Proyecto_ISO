8831eab9a432995c3a2b3267f8b865c14d92fb10
pixelpercebe <icalbo360@gmail.com>
2024-02-28 12:39:55 +0100

    ficheros reorganizados y .gitignore añadido

---
+++ verificaP05-2.sh
@@ -0,0 +1,15 @@
+# Verificar si la carpeta pruebas existe
+if [ ! -d "pruebas" ]; then
+    # Si no existe, crearla
+    mkdir pruebas
+    echo "Carpeta 'pruebas' creada."
+else
+    echo "La carpeta 'pruebas' ya existe."
+fi
+echo "Hola  que tal..." > pruebas/f1.dat ; seq 1 1000 >> pruebas/f1.dat
+
+./mysha256-repo I pruebas/f1.dat  repositorio1.rep
+
+sha256sum  pruebas/f1.dat
+
+hexdump  -C repositorio1.rep |head  -20
