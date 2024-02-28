60b70b157f711cdc78d160ab3e04d430623afc06
pixelpercebe <icalbo360@gmail.com>
2024-02-28 12:15:40 +0100

    archivos añadidos desde el servidor

---
+++ code/insertar_fichero.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h> // Para lseek y close
+
+#include "s_my_sha256header.h"
+#include "codes.h"
+
+// function that opens a RepoFile with O_append
+int insertar_fichero(char RepoFileName[256])
+{
+    int fd_RepoFile = open(RepoFileName, O_WRONLY | O_CREAT | O_APPEND, 0600);
+    if (fd_RepoFile == -1)
+    {
+    fprintf(stderr, "Cannot open file RepoFileName %s\n", RepoFileName);
+    return ERROR_OPEN_SHA_REPO_FILE;
+    }
+    return fd_RepoFile;
+}
+
+
+// function that opens a RepoFile with lseek and header.
+int insertar_fichero_lseek(char RepoFileName[256], struct c_sha256header *header) {
+    int fd_RepoFile = open(RepoFileName, O_WRONLY | O_CREAT, 0600);
+    if (fd_RepoFile == -1) 
+    {
+        fprintf(stderr, "Cannot open file RepoFileName %s\n", RepoFileName);
+        return ERROR_OPEN_SHA_REPO_FILE;
+    }
+    // Move the pointer to the end of the 'RepoFile' file to ensure that the content is appended to the end.
+    if (lseek(fd_RepoFile, 0, SEEK_END) == (off_t)-1) {
+        fprintf(stderr, "ERROR WITH LSEEK %d\n", E_DESCO);
+        return E_DESCO;
+    }
+
+    if (RepoFileName == NULL || RepoFileName[0] == '\0') {
+        fprintf(stderr, "Invalid file RepoName.\n");
+        return ERROR_OPEN_FILE;
+    }
+    return fd_RepoFile; //exito
+}
---
+++ verificaP05.sh
@@ -0,0 +1,17 @@
+
+# Verificar si la carpeta pruebas existe
+if [ ! -d "pruebas" ]; then
+    # Si no existe, crearla
+    mkdir pruebas
+    echo "Carpeta 'pruebas' creada."
+else
+    echo "La carpeta 'pruebas' ya existe."
+fi
+echo "Hello how are you...This is  a new test" > pruebas/f1.dat ; seq 1 1000 >> pruebas/f1.dat
+
+# execute mysha256-repo to Insert a file (I)  ("I" is a new argument of mysha256-repo.)
+./mysha256-repo  I  pruebas/f1.dat repositorio1.rep
+
+sha256sum  pruebas/f1.dat
+
+hexdump -C repositorio1.rep | less
